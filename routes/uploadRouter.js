9c308ee5308cf0af55d576f5654c69d271b7803d
dbto1970 <donbartojunior@gmail.com>
2021-10-19 16:45:14 -0400

    file upload successful

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -12,13 +12,13 @@ const storage = multer.diskStorage({
 });
 
 const imageFileFilter = (req, file, cb) => {
-    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
         return cb(new Error('You can upload only image files!'), false);
     }
     cb(null, true);
 };
 
-const upload = multer({storage: storage, fileFilter: imageFileFilter});
+const upload = multer({ storage: storage, fileFilter: imageFileFilter });
 
 const uploadRouter = express.Router();
 
@@ -27,18 +27,23 @@ uploadRouter.route('/')
     res.statusCode = 403;
     res.end('GET operation not supported on /imageUpload');
 })
-.post(authenticate.verifyUser, authenticate.verifyAdmin, upload.single('imageFile'), (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(req.file);
-})
+.post(
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    upload.single('imageFile'),
+       (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(req.file);
+    }
+)
 .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /imageUpload');
 })
 .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
     res.statusCode = 403;
-    res.end('Delete operation not supported on /imageUpload');
+    res.end('DELETE operation not supported on /imageUpload');
 });
 
 module.exports = uploadRouter;
